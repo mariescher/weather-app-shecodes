5d419a97464da1108688411ff675e5ea5f5e0315
mariescher <mari.escher@gmail.com>
2024-09-23 07:33:37 +0100

    Added default city

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function refreshTemperature(response) {
   let temperatureElement = document.querySelector("#weather-app-temperature");
   let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
 
   temperatureElement.innerHTML = Math.round(temperature);
 }
@@ -14,10 +16,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
